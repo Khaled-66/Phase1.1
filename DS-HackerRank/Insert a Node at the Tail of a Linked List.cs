6c65a0e5b48c9af89e560edea9cd0f487d4da362
Khaled-66 <ashrafkhaled860@gmail.com>
2025-02-02 18:10:50 +0200

    p6

---
+++ DS-HackerRank/Insert a Node at the Tail of a Linked List.cs
@@ -0,0 +1,18 @@
+﻿static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data)
+{
+    SinglyLinkedListNode s1 = new SinglyLinkedListNode(data);
+    SinglyLinkedListNode temp = head;
+    if (head == null)
+    {
+        return s1;
+    }
+    else
+    {
+        while (temp.next != null)
+        {
+            temp = temp.next;
+        }
+        temp.next = s1;
+        return head;
+    }
+}
