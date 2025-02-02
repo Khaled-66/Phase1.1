5ec0e6599bc92159c6d2556c8cb819d03b7323f1
Khaled-66 <ashrafkhaled860@gmail.com>
2025-02-02 08:49:58 +0200

    first

---
+++ DataStructures/2Darray-DS.cs
@@ -0,0 +1,17 @@
+﻿public static int hourglassSum(List<List<int>> arr)
+{
+    int s = arr.Count;
+    int max = -2147483648;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            int r = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+            if (r > max)
+            {
+                max = r;
+            }
+        }
+    }
+    return max;
+}
---
+++ DataStructures/Array-DS.cs
@@ -0,0 +1,12 @@
+﻿public static List<int> reverseArray(List<int> a)
+{
+    int n = a.Count;
+    for (int i = 0; i < n / 2; i++)
+    {
+        int temp = a[i];
+        a[i] = a[n - i - 1];
+        a[n - i - 1] = temp;
+
+    }
+    return a;
+}
