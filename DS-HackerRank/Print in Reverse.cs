a9522bd5800e6df9b5f5892d349637f47b197be8
Khaled-66 <ashrafkhaled860@gmail.com>
2025-02-03 14:00:01 +0200

    p10

---
+++ DS-HackerRank/Print in Reverse.cs
@@ -0,0 +1,15 @@
+﻿public static void reversePrint(SinglyLinkedListNode llist)
+{
+    SinglyLinkedListNode temp = llist;
+    Stack<int> ints = new Stack<int>();
+    while (temp != null)
+    {
+        ints.Push(temp.data);
+        temp = temp.next;
+    }
+    while (ints.Count > 0)
+    {
+        Console.WriteLine(ints.Pop());
+    }
+
+}
