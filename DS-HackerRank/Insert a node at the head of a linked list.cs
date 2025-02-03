2a8ea82a66c499e0f9f1155c53f8fe5157eff4a4
Khaled-66 <ashrafkhaled860@gmail.com>
2025-02-03 12:06:03 +0200

    p7

---
+++ DS-HackerRank/Insert a node at the head of a linked list.cs
@@ -0,0 +1,9 @@
+﻿static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int data)
+{
+    SinglyLinkedListNode temp = new SinglyLinkedListNode(data);
+    temp.next = null;
+    temp.next = llist;
+    llist = temp;
+    return temp;
+
+}
