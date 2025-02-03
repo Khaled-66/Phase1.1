58db906a615b0a0b5b9529c92ee38c315908eadb
Khaled-66 <ashrafkhaled860@gmail.com>
2025-02-03 12:36:41 +0200

    p8

---
+++ DS-HackerRank/Insert a node at a specific position in a linked list.cs
@@ -0,0 +1,20 @@
+﻿static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode llist, int data, int position)
+{
+    SinglyLinkedListNode n1 = new SinglyLinkedListNode(data);
+    if (llist == null)
+    {
+        return n1;
+    }
+    else
+    {
+        SinglyLinkedListNode temp = llist;
+        for (int i = 0; i < position - 1 && temp != null; i++)
+        {
+            temp = temp.next;
+        }
+        n1.next = temp.next;
+        temp.next = n1;
+
+    }
+    return llist;
+}
